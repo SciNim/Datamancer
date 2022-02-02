4b5457e89bb5b76dc01b53463638a04b908eb0fd
Vindaar <basti90@gmail.com>
2022-02-02 20:26:30 +0100

    make DataFrame a generic type of ColumnLike types

--- src/datamancer/df_types.nim
+++ src/datamancer/df_types.nim
@@ -7,9 +7,9 @@ type
 
   # where value is as usual
   # then
-  DataFrame* = ref object
+  DataFrame*[T] = ref object
     len*: int
-    data*: OrderedTable[string, Column]
+    data*: OrderedTable[string, T] ## `T` is the column kind
     case kind*: DataFrameKind
     of dfGrouped:
       # a grouped data frame stores the keys of the groups and maps them to
