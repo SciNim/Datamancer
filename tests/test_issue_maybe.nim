3d75e2437411146aaf1641ffd430d98988ae0dee
Vindaar <basti90@gmail.com>
2022-06-01 15:11:54 +0200

    when writing tests is not part of your skills

--- tests/test_issue_maybe.nim
+++ tests/test_issue_maybe.nim
@@ -3,4 +3,4 @@ let df = seqsToDf({"x": @[1, 2, 3, 4, 5], "y": @[5, 10, 15, 20, 25]})
 try:
   echo df.mutate(f{float: "ratio" ~ `x`/sum(`x`)}).summarize(f{float: mean(`field_that_does_not_exist`)})
 except KeyError:
-  continue # we expect a KeyError. Do we get an AssertionError on some platforms / nim versions?
+  discard # we expect a KeyError. Do we get an AssertionError on some platforms / nim versions?
