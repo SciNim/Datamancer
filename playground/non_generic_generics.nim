3ae580e6580c695253b036bc6ef76f4319b4f21a
Vindaar <basti90@gmail.com>
2022-08-05 17:42:55 +0200

    clean up and extend non generics example

--- playground/non_generic_generics.nim
+++ playground/non_generic_generics.nim
@@ -1,19 +1,24 @@
-import unchained, measuremancer
-
 import macros
 import arraymancer
-import datamancer # / [column, value]
+import datamancer
 
-#patchColumn(Tensor[Measurement[float]])
-import tables, sets # not needed if not called from here `patchDataFrame`
 
+# for testing here
+import unchained, measuremancer
+
+
+## Put the overloadable enums into your `nim.cfg`!
 {.experimental: "overloadableEnums".}
 
-proc makeCol[T](s: seq[T]) =
-  var c = toColumn(s.toTensor)
-  echo pretty(c)
+proc `%~`[T](x: Measurement[T]): Value =
+  result = %~ ($x)
+  # or
+  #result = newVobject()
+  #result["val"] = %~ x.value
+  #result["uncer"] = %~ x.error
+  # depending on if one wants it only for printing or more. Former
+  # is better to parse back
 
-#var t = toTensor(@[2.kg])
 defColumn(KiloGram)
 defColumn(Measurement[float])
 block:
@@ -34,30 +39,32 @@ block:
   c[0] = 1.5 ± 0.2
   echo c
 
+
+# to test type logic works in a generic:
+proc makeCol[T](s: seq[T]) =
+  var c = toColumn(s.toTensor)
+  echo pretty(c)
+
 # good, also works in a generic
 let x = @[1.0 ± 0.1, 2.0 ± 0.5]
 makeCol(x)
 
 echo "================================================================================"
-#patchDataFrame(Tensor[KiloGram])
+
+# define a function using the `dfFn` macro that takes a `DF` to deduce the
+# required input / return type of the formula
 let df = seqsToDf({"x" : [1,2,3]})
 let fn = dfFn(df, f{float -> KiloGram: "y" ~ (`x` * `x`).kg})
 echo typeof(fn)
 
 defUnit(kg²)
-#genTypeEnum(KiloGram²)
-#type kg2Col = patchColumn(KiloGram²)
 defColumn(Meter)
-#genTypeEnum(Meter)
-#type mCol = patchColumn(Meter)
 
-# needs multi generics. to be impl'd next
-#let dfNM = dfN.mutate(f{KiloGram -> KiloGram²: "kg2" ~ `kg` * `kg`})
-
-
+# more examples of what's possible
 let dfN = extendDataFrame(df, "kg", @[3.kg, 1.kg, 2.kg])
 echo dfN.pretty(precision = 10)
 
+# sorting works
 echo dfN.arrange("kg", SortOrder.Descending).pretty(precision = 10)
 echo dfN.arrange("kg", SortOrder.Ascending).pretty(precision = 10)
 
@@ -66,8 +73,6 @@ let fn2 = dfFn(dfN, f{KiloGram -> KiloGram²: "kg2" ~ `kg` * `kg`})
 echo typeof(fn2)
 #
 defColumn(KiloGram, Meter)
-##genTypeEnum(KiloGram, Meter)
-##type ttCol = patchColumn(KiloGram, Meter)
 let dfNM = extendDataFrame(dfN, "meter", @[1.m, 2.m, 3.m])
 echo dfNM.pretty(precision = 10)
 
@@ -78,6 +83,17 @@ echo dfNM.mutate(dfFn(dfNM, f{float: "kgtofoat" ~ idx(`kg`, KiloGram).float}))
 echo dfNM.mutate2(f{float: "kgtofoat" ~ idx(`kg`, KiloGram).float})
 
 
+block:
+  # `to` can be used to change DF type
+  doAssert df is DataFrame
+  let dfAsKg = df.to(DataTable[colType(KiloGram)])
+  doAssert type(dfAsKg) is DataTable[colType(KiloGram)]
+  # or
+  let dfAsKg2 = df.to(colType(KiloGram))
+  doAssert type(dfAsKg2) is DataTable[colType(KiloGram)]
+
+# complex types work (as long as they have simple fields, nothing
+# further needed)
 type
   Foo = object
     x: string
@@ -85,13 +101,45 @@ type
 proc `<`(f1, f2: Foo): bool = f1.val < f2.val
 defColumn(Foo)
 # accumulating types works to call mutate w/o df arg
-var dFoo = colType(Foo).newDataTable()
-dFoo["bar"] = @[Foo(x: "hello", val: 1.1), Foo(x: "world", val: 100.5)]
-dFoo = dFoo.mutate(f{Foo -> float: "values" ~ `bar`.val})
-echo dFoo.pretty(precision = 30)
+let fooSeq = @[Foo(x: "hello", val: 1.1), Foo(x: "world", val: 100.5)]
+
+block:
+  var dFoo = colType(Foo).newDataTable()
+  dFoo["bar"] = fooSeq
+  dFoo = dFoo.mutate(f{Foo -> float: "values" ~ `bar`.val})
+  echo dFoo.pretty(precision = 30)
 
 # extend simply by accumulating types
 ## WARNING: generating a column for a type that is an alias may cause problems!
 defColumn(KiloGram²)
-let df2 = df.mutate(f{int: "kg²" ~ `x`.kg²})
-echo df2
+block:
+  let df2 = df.mutate(f{int: "kg²" ~ `x`.kg²})
+  echo df2
+
+block:
+  # can construct using `toDf` call
+  let df2 = toDf(fooSeq)
+  echo df2
+
+block:
+  # still works with two
+  let y = @[1, 2]
+  let df2 = toDf(fooSeq, y)
+  echo df2
+
+block:
+  # also with a single named argument
+  let df2 = toDf({"x" : fooSeq})
+  echo df2
+
+block:
+  # and with Measurement[float]
+  let df2 = toDf({"x" : x})
+  echo df2
+
+
+block:
+  # and of course mixed
+  let y = @[1, 2]
+  let df2 = toDf({"x" : x, "y" : y})
+  echo df2
